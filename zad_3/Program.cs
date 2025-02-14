022732ace1071027507bfc0d7e7f87b05ffcd7b1
Filipinkus <filip11.house@gmail.com>
2025-02-14 17:13:04 +0200

    Add files via upload

---
+++ zad_3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace zad_3
+{
+    internal class Program
+    {
+        static int Reverse(int n)
+        {
+            if (n < 10)
+            {
+                return n;
+            }
+            else
+            {
+                return int.Parse(n % 10 + "" + Reverse(n / 10));
+            }
+        }
+
+        static void Main()
+        {
+            int number = 911;
+            int reversedNumber = Reverse(number);
+            Console.WriteLine(reversedNumber);  
+        }
+    }
+}
