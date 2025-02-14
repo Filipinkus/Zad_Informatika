8fc7e6b80ff30fb1bcaeea9198d319d55b2bcb20
Filipinkus <filip11.house@gmail.com>
2025-02-14 16:46:09 +0200

    Add files via upload

--- spi mi se/Program.cs
+++ spi mi se/Program.cs
@@ -4,26 +4,33 @@
     {
         static void Main()
         {
-            int n = 16;
+            int n = 9;
             bool result = IsPowerOfTwo(n);
-            Console.WriteLine(result ? $"{n} е степен на 2." : $"{n} не е степен на 2.");
+            if (result)
+            {
+                Console.WriteLine(n + " е степен на 2.");
+            }
+            else
+            {
+                Console.WriteLine(n + " не е степен на 2.");
+            }
         }
 
         static bool IsPowerOfTwo(int n)
-        {
-            // Ако числото е по-малко или равно на 0, не е степен на 2
+        {            
             if (n <= 0)
+            {
                 return false;
-
-            // Ако числото е 1, то е степен на 2
+            }
+                      
             if (n == 1)
+            {
                 return true;
-
-            // Ако числото не е делимо на 2, не е степен на 2
+            }       
             if (n % 2 != 0)
+            {
                 return false;
-
-            // Рекурсивно проверяваме дали половината от числото също е степен на 2
+            }
             return IsPowerOfTwo(n / 2);
         }
     }
